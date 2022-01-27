fa5feb1f826c13920d7af8b01f2d694c7752c0ce
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-01-26 21:10:25 -0300

    Obter produto

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -26,6 +26,35 @@ const listarProdutos = async (req, res) => {
     }
 }
 
+const obterProduto = async (req, res) => {
+    const { usuario } = req;
+    const { id: produtoId } = req.params;
+
+    if (!usuario) {
+        return res.status(401).json({ "mensagem": "Para acessar este recurso um token de autenticação válido deve ser enviado." });
+    }
+
+    try {
+        const queryObterProduto = "select * from produtos where id = $1";
+        const produtos = await conexao.query(queryObterProduto, [produtoId]);
+
+        if (produtos.rowCount === 0) {
+            return res.status(404).json({ "mensagem": `Não existe produto cadastrado com ID ${produtoId}.` });
+        }
+
+        const produto = produtos.rows[0];
+
+        if (produto.usuario_id !== usuario.id) {
+            return res.status(403).json({ "mensagem": "O usuário logado não tem permissão para acessar este produto." });
+        }
+
+        return res.status(200).json(produto);
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+}
+
+
 module.exports = {
     listarProdutos,
     obterProduto,
