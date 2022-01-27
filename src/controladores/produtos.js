52045727c0a2382b769785b27400fd06a1d59b36
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-01-26 21:12:29 -0300

    Atualizar produto

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -103,6 +103,64 @@ const cadastrarProduto = async (req, res) => {
     }
 }
 
+const atualizarProduto = async (req, res) => {
+    const { usuario } = req;
+    const { id: produtoId } = req.params;
+    const { nome, quantidade, categoria, preco, descricao, imagem } = req.body;
+
+    if (!usuario) {
+        return res.status(401).json({ "mensagem": "Para atualizar um produto, o usuário deve estar autenticado." });
+    }
+    if (!nome) {
+        return res.status(400).json({ "mensagem": "O campo nome é obrigatório" });
+    }
+    if (!quantidade) {
+        return res.status(400).json({ "mensagem": "O campo quantidade é obrigatório" });
+    }
+    if (!preco) {
+        return res.status(400).json({ "mensagem": "O campo preco é obrigatório" });
+    }
+    if (!descricao) {
+        return res.status(400).json({ "mensagem": "O campo descricao é obrigatório" });
+    }
+
+    try {
+        const queryObterProduto = "select * from produtos where id = $1";
+        const produtos = await conexao.query(queryObterProduto, [produtoId]);
+
+        if (produtos.rowCount === 0) {
+            return res.status(404).json({ "mensagem": `Não existe produto cadastrado com ID ${produtoId}.` });
+        }
+        
+        const produto = produtos.rows[0];
+
+        if (produto.usuario_id !== usuario.id) {
+            return res.status(403).json({ "mensagem": "O usuário logado não tem permissão para atualizar este produto." });
+        }
+        const queryAtualizarProduto = `update produtos set 
+            nome = $1,
+            quantidade = $2,
+            categoria = $3,
+            preco = $4,
+            descricao = $5,
+            imagem = $6
+            where id = $7
+        `;
+        const produtoAtualizado = await conexao.query(queryAtualizarProduto,
+            [nome, quantidade, categoria, preco, descricao, imagem, produtoId]
+        );
+        
+        if (produtoAtualizado.rowCount === 0) {
+            return res.status(400).json({ "mensagem": "Não foi possível atualizar o produto" });
+        }
+        
+        return res.status(200).json({"mensagem":"Produto atualizado com sucesso."});
+    } catch (error) {
+        console.log("chegou2");
+        return res.status(400).json(error);
+    }
+}
+
 
 
 module.exports = {
