3c25cf94e11ab79bd5060c2a201a7ec10658699d
AdelinoJonas <84252481+AdelinoJonas@users.noreply.github.com>
2022-01-26 21:05:04 -0300

    Atualizar usuário

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -119,6 +119,59 @@ const obterUsuario = async (req, res) => {
     
 }
 
+const atualizarUsuario = async (req, res) => {
+    const token = req.header('authorization');
+    const { nome, email, senha, nome_loja } = req.body;
+
+    if (!token) {
+        return res.status(401).json({ "mensagem": "Para acessar este recurso um token de autenticação válido deve ser enviado." });
+    }
+    if (!nome) {
+        return res.status(400).json({ "mensagem": "O campo nome é obrigatório" });
+    }
+    if (!email) {
+        return res.status(400).json({ "mensagem": "O campo email é obrigatório" });
+    }
+    if (!senha) {
+        return res.status(400).json({ "mensagem": "O campo senha é obrigatório" });
+    }
+    if (!nome_loja) {
+        return res.status(400).json({ "mensagem": "O campo nome_loja é obrigatório" });
+    }
+
+    const id = req.params.id;
+
+    try {
+        const queryVericacaoEmail = "select * from usuarios where email = $1 and id = $2";
+        const usuarios = await conexao.query(queryVericacaoEmail, [email, id]);
+
+        if (usuarios.rowCount > 0) {
+            return res.status(400).json({ "mensagem": "Já existe usuário cadastrado com o e-mail informado." });
+        }
+
+        const senhaCriptografada = await bcrypt.hash(senha, 10);
+
+        const queryAtualizarUsuario = `update usuarios set
+            nome = $1,
+            email = $2,
+            senha = $3,
+            nome_loja = $4
+            where id = $5
+        `;
+        const usuarioAtualizado = await conexao.query(queryAtualizarUsuario,
+            [nome, email, senhaCriptografada, nome_loja, id]);
+
+        if (usuarioAtualizado.rowCount === 0) {
+            return res.status(400).json({ "mensagem": "Não foi possível atualizar o usuário" });
+        }
+
+        return res.status(204).json();
+    } catch (error) {
+        return res.status(400).json(error);
+    }
+
+}
+
 
 
 module.exports = {
